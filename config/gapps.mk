150749bb0435b661261b593ebbfe7e009c5c0fa4
Bicet <bicets@gmail.com>
2021-11-04 12:08:32 +0100

    Use Proton GAPPS

--- config/gapps.mk
+++ config/gapps.mk
@@ -1,14 +1,14 @@
 # Gapps
 ifeq ($(WITH_GAPPS),true)
-$(call inherit-product, vendor/gms/gms_full.mk)
+$(call inherit-product, vendor/gapps/common/common-vendor.mk)
 
-# Common Overlay
-DEVICE_PACKAGE_OVERLAYS += \
-    vendor/iron/overlay-gapps/common
+# # Common Overlay
+# DEVICE_PACKAGE_OVERLAYS += \
+#     vendor/iron/overlay-gapps/common
 
-# Exclude RRO Enforcement
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS +=  \
-    vendor/iron/overlay-gapps
+# # Exclude RRO Enforcement
+# PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS +=  \
+#     vendor/iron/overlay-gapps
 
-$(call inherit-product, vendor/iron/config/rro_overlays.mk)
+# $(call inherit-product, vendor/iron/config/rro_overlays.mk)
 endif
