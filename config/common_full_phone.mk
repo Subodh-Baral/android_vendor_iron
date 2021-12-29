1281bc69bb45c43d1f2974365345261350deff55
ralf979 <ralf979@gmail.com>
2021-12-29 11:17:21 +0000

    vendor: fixup World APN list

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -8,4 +8,8 @@ PRODUCT_PACKAGES += \
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/iron/overlay/dictionaries
 
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/iron/prebuilt/common/etc/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+
 $(call inherit-product, vendor/iron/config/telephony.mk)
